362cd016fdd26deb1ba9dfcf86507abd7a105047
lucifer <604324055@qq.com>
2016-11-28 17:38:10 +0800

    feat: 启动页显示倒计时

--- app/src/main/java/com/team/zhuoke/masterhelper/SplashActivity.java
+++ app/src/main/java/com/team/zhuoke/masterhelper/SplashActivity.java
@@ -2,9 +2,11 @@ package com.team.zhuoke.masterhelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.TextView;
 
 import com.team.zhuoke.masterhelper.activity.MainActivity;
 import com.team.zhuoke.masterhelper.utils.SharedPreferenceCommen;
@@ -17,16 +19,30 @@ import com.team.zhuoke.masterhelper.widget.PathView;
 public class SplashActivity extends AppCompatActivity {
     PathView mPathView;
     private static final int COUNTDOWN = 3000;
+    private CountDownTimer mTimer;
+    private TextView mCountDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         mPathView = (PathView) findViewById(R.id.pathView);
+        mCountDown=(TextView) findViewById(R.id.tv_countDown);
         initView();
     }
 
     private void initView() {
+        mTimer=new CountDownTimer(COUNTDOWN,1000) {
+            @Override
+            public void onTick(long l) {
+                mCountDown.setText(""+l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                mCountDown.setText("0");
+            }
+        };
         boolean firstblood = SharedPreferenceCommen.getFristBliid(SharedPreferenceCommen.FIRSTBLOOD);
         if (firstblood) {
             // TODO: 2016/11/22 引导页
@@ -45,6 +61,7 @@ public class SplashActivity extends AppCompatActivity {
                 duration(2000).
                 interpolator(new AccelerateDecelerateInterpolator()).
                 start();
+        mTimer.start();
         new Handler().postDelayed(() -> next(), COUNTDOWN);
     }
 
