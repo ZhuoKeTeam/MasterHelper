f8da9c3c49ba39fd6f9f461583f47105b2b331d5
wangqing <wangqing@kaolafm.com>
2016-12-09 16:59:34 +0800

    1. 添加测试 MVP界面3.

---
+++ app/src/main/java/wq/gdy005/mvp/TestActivity3.java
@@ -0,0 +1,60 @@
+package wq.gdy005.mvp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.team.zhuoke.masterhelper.R;
+import com.team.zhuoke.masterhelper.base.BaseActivity;
+import com.team.zhuoke.masterhelper.base.BaseView;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+/**
+ * Created by WangQing on 2016/12/9.
+ */
+
+public class TestActivity3 extends BaseActivity<Test3Model, Test3Presenter> implements Test3Contract.View {
+    @InjectView(R.id.edit_text)
+    EditText editText;
+    @InjectView(R.id.btn_home)
+    Button btnHome;
+
+    @Override
+    public void setData(String s) {
+
+        editText.setText(s);
+    }
+
+    @Override
+    public void setErrorInfo(String message) {
+        editText.setText(message);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test3;
+    }
+
+    @Override
+    protected void onInitView(Bundle bundle) {
+
+    }
+
+    @Override
+    protected void onEvent() {
+
+    }
+
+    @Override
+    protected BaseView getView() {
+        return this;
+    }
+
+
+    @OnClick(R.id.btn_home)
+    public void onClick() {
+        mPresenter.getTestList();
+    }
+}
